effd9c7fc9ddd3a1ebce83c1fe79e5633f54b912
mrbinder <77510963+mrbinder@users.noreply.github.com>
2022-01-28 01:39:44 -0800

    Update request.php

--- php/request.php
+++ php/request.php
@@ -6,10 +6,14 @@ function send_post($url,$data){
     while (true){
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $headers = [
+            'content-type: application/json; charset=UTF-8',
+        ];
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $response = curl_exec($ch);
-        global $ngix_error;
-        if ($response != null & strpos($response,$ngix_error) == false){
+        global $ngix_error_bypass;
+        if ($response != null & strpos($response,$ngix_error_bypass) == false){
             break;
         }
     }
